cce4ad65aff91470e0f5483aebe6a36455b376c8
Matt <94744945+madscs1@users.noreply.github.com>
2022-03-14 08:14:55 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 function opponentNoteHit()
     health = getProperty('health')
-    if getProperty('health') > 0.2 then
+    if getProperty('health') > 0.1 then
         setProperty('health', health- 0.02);
     end
 end
